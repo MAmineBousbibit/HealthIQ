4ae9271bf78c15dce6f5b1acd509f2f1180a242d
Zakaria022 <125134307+Zakaria022@users.noreply.github.com>
2023-11-20 18:02:58 +0100

    zakaria

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+
+// Import Angular Material modules
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { ContactComponent } from './contact/contact.component';
+// Import other necessary Angular Material modules
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    ContactComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatFormFieldModule, // Add Angular Material modules here
+    MatInputModule,
+    MatSelectModule,
+    // Add other required Angular Material modules
   ],
   providers: [],
   bootstrap: [AppComponent]
