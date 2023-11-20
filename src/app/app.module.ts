4bc1db00e52efc5917dc0e08ab0b09e02174e341
Khouloud Bolif <khouloudbolif@gmail.com>
2023-11-20 17:52:01 +0100

    Blogs and statistiq

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MapsComponent } from './Views/maps/maps.component';
+import { BlogsComponent } from './Views/blogs/blogs.component';
+import { StatistiqueComponent } from './Views/statistique/statistique.component';
+import { BannerComponent } from './Views/banner/banner.component';
+//import { AgmCoreModule } from '@agm/core';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapsComponent,
+    BlogsComponent,
+    StatistiqueComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+   // AgmCoreModule.forRoot({
+   // apiKey='AIzaSyCsDB4BgPFsmAUX5O2UluvYtZN8YUHpX8M'
+  // })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+
+}
