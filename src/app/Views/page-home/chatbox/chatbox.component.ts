ba062869ce1bc08efaef71f1912e8af63f8ae849
Khouloud Bolif <khouloudbolif@gmail.com>
2023-12-26 03:10:50 +0100

    connect chatbot with chatGPT's api

--- src/app/Views/page-home/chatbox/chatbox.component.ts
+++ src/app/Views/page-home/chatbox/chatbox.component.ts
@@ -1,17 +1,30 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-chatbox',
   templateUrl: './chatbox.component.html',
   styleUrls: ['./chatbox.component.css']
 })
-export class ChatboxComponent {
-
+export class ChatboxComponent implements OnInit {
+  queryFormGroup!:FormGroup
+  messages=[
+    
+    {role:"system",content : "hello"}
+  ]
+  result:any
   box = true;
   chat=false;
 
   inputValue: string = ''; 
-  
+
+  constructor(private formBuilder:FormBuilder,private httpClient:HttpClient){}
+ ngOnInit(): void {
+     this.queryFormGroup=this.formBuilder.group({
+      query:this.formBuilder.control('')
+     })
+ } 
   toggleChatBox(): void {
     this.box=false;
     this.chat=true;
@@ -26,6 +39,34 @@ sendmsg(){
   this.inputValue='';
 }
 
+handleAskGPT(){
+let url="https://api.openai.com/v1/chat/completions"
+let httpHeaders=new HttpHeaders().set("Authorization","Bearer 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")
+this.messages.push({
+  role:"user",content:this.queryFormGroup.value.query
+})
+let payload=
+  {
+    model:"gpt-3.5-turbo",
+    messages:this.messages
+  }
 
+this.httpClient.post(url,payload,{headers:httpHeaders}).subscribe({
+  next:(resp)=>{
+   this.result=resp
+   this.result.choices.forEach((choice:any)=>{
+     this.messages.push({
+      role:"assistant",content:choice.message.content
+   })
+   })
+   
+   console.log(this.result)
+  },
+  error:(err)=>{
+
+  }
+})
+
+}
 }
 
