35bc04ccb3315a0ec53034707317c589e9d994da
Zakaria022 <125134307+Zakaria022@users.noreply.github.com>
2024-01-22 16:17:43 +0100

    profile

--- src/app/Views/tabset-patient/tabset-patient.component.ts
+++ src/app/Views/tabset-patient/tabset-patient.component.ts
@@ -1,10 +1,51 @@
 import { Component } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-tabset-patient',
   templateUrl: './tabset-patient.component.html',
   styleUrls: ['./tabset-patient.component.css']
 })
 export class TabsetPatientComponent {
+  specialites: String[] = ["Speciality 1", "Speciality 2", "Speciality 3", "Speciality 4"]
+  doctors: String[] = ["doctor 1", "doctor 2", "doctor 3", "doctor 4"]
+  title = 'Bytes';
+  activeTab:string = 'Personal Details';
+  appointmentForm: FormGroup;
+   onTabClick(tab: string){
+     this.activeTab = tab;
+   }
 
+   selectOption(option: String) {
+    //console.log('Option sélectionnée sanguin groupe : ', option);
+    this.appointmentForm.get('thirdForm.group_sang')?.setValue(option)
+
+  }
+  
+   constructor(private formBuilder: FormBuilder) {
+    this.activeTab = 'Personal Details';
+    this.appointmentForm = this.formBuilder.group({
+      firstname: ['', Validators.required],
+      lastname: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      phone: ['', Validators.required],
+      date: ['', Validators.required],
+      time: ['', Validators.required],
+      speciality: ['', Validators.required],
+      doctor: ['', Validators.required],
+      status: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if (this.appointmentForm.valid) {
+      const formValues = this.appointmentForm.value;
+      const selectedSpeciality = formValues.speciality;
+      const selectedDoctor = formValues.doctor;
+      // Handle form submission here
+      console.log(formValues);
+      console.log("Selected Speciality: ", selectedSpeciality);
+      console.log("Selected Doctor: ", selectedDoctor);
+    }
+  }
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MatTabsModule } from '@angular/material/tabs';
 
 
 import { FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -91,7 +92,7 @@ import { SearchPipeHomePipe } from './Views/navbar/search-pipe-home.pipe';
     FormsModule,
     FullCalendarModule,
     ModalModule.forRoot(),
-    
+    MatTabsModule,
     ReactiveFormsModule,
     HttpClientModule,
     RouterModule,
