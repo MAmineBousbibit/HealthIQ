69eec1356dbca6f67175c45cb79814dd6de8774f
Zakaria022 <125134307+Zakaria022@users.noreply.github.com>
2024-01-24 00:18:40 +0100

    pop up patient

--- src/app/Views/navbar/navbar.component.ts
+++ src/app/Views/navbar/navbar.component.ts
@@ -7,38 +7,35 @@ import { AuthService } from 'src/app/_Services/auth.service';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
-  constructor(private authService: AuthService) {
-  this.isLoggedIn()
-  }
+
+  isTabsetVisible = false;
   islogin = false;
   User_ID:any
-  isLoggedIn() {
-     this.authService.isAuthenticated;
-    this.User_ID= this.authService.getIDUser()
-    console.log("idUser",this.User_ID);
-    
-   if(this.User_ID != undefined){
-    this.islogin=true
-   }
-    
-    
-  }
-
-  login() {
-    // Ajoutez ici votre logique de connexion (peut-être un appel à une méthode du service d'authentification)
-  }
-
-  logout() {
-    // Ajoutez ici votre logique de déconnexion (peut-être un appel à une méthode du service d'authentification)
-  }
-
   searchText='';
-
-  // Replace with your logic for login status
- // Replace with your logic for DoctorData
   dropdownOpen = false;
+  Id=""
 
-  toggleDropdown() {
-    this.dropdownOpen = !this.dropdownOpen;
+  constructor(private authService: AuthService) {
+    this.isLoggedIn()
+    }
+    
+   toggleDropdown() {
+      this.dropdownOpen = !this.dropdownOpen;
+    }
+
+  toggleTabsetVisibility() {
+    this.isTabsetVisible = !this.isTabsetVisible;
   }
+
+  isLoggedIn() {
+    this.authService.isAuthenticated;
+   this.User_ID= this.authService.getIDUser()
+   console.log("idUser",this.User_ID);
+   
+  if(this.User_ID != undefined){
+   this.islogin=true
+  }
+ }
+
+ 
 }
