ac3eaf61a4a633de678897422516ded89c3c2f35
MAmineBousbibit <mbousbibit@gmail.com>
2024-01-25 03:15:23 +0100

    notification admin and soc

--- src/app/SuperAdmin/navbar-admin/navbar-admin.component.ts
+++ src/app/SuperAdmin/navbar-admin/navbar-admin.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import {Component, NgModule} from '@angular/core';
 import { AuthService } from 'src/app/_Services/auth.service';
 import { UserService } from 'src/app/_Services/user.service';
 import { User } from 'src/app/_models/user';
+import {NgbPopoverModule} from "@ng-bootstrap/ng-bootstrap";
+import {Notification} from "../../_models/Notification";
+import {NotificationService} from "../../_Services/Notification.service";
 
 @Component({
   selector: 'app-navbar-admin',
@@ -11,17 +14,53 @@ import { User } from 'src/app/_models/user';
 export class NavbarAdminComponent {
   AdminData=new User()
   ID:any
-  constructor(private AuthService:AuthService ,private UserSevice:UserService){
+
+  @NgModule({
+    imports: [
+      // ... other imports
+      NgbPopoverModule,
+    ],
+    // ... other module configurations
+  })
+
+
+  isNotificationDropdownOpen: boolean = false;
+  isReadMoreClicked: boolean = false;
+  notifications: Notification[] = [];
+  constructor(private AuthService:AuthService ,private UserSevice:UserService,private notificationService:NotificationService){
    this.ID=this.AuthService.getIDUser()
  //console.log("Admin Id ", this.ID);
  this.UserSevice.getOneUser( this.ID).subscribe(
   (rep:any)=>{
     this.AdminData=rep
   //  console.log("admindaata",this.AdminData);
-    
+
   }
  )
- 
+
+  }
+  toggleNotificationDropdown() {
+    this.isNotificationDropdownOpen = !this.isNotificationDropdownOpen;
+  }
+
+
+
+
+
+  onClickNotificationButton(): void {
+    // Call the service method to get notifications
+    this.notificationService.findAll().subscribe((notifications) => {
+      // Handle the notifications as needed (e.g., update a property to display them)
+      this.notifications = notifications;
+      // Toggle the dropdown to open
+      this.toggleNotificationDropdown();
+    });
+  }
+
+  onDeleteNotification(notificationId: string): void {
+    this.notificationService.deleteById(notificationId).subscribe(() => {
+      this.notifications = this.notifications.filter(n => n.id !== notificationId);
+    });
   }
 
 }
